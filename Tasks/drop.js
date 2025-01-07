bf7d34ca1e891b88decca9583d65c921516bf18f
loneliness <129841705+SkVaYeR228@users.noreply.github.com>
2025-01-07 14:40:25 +0200

    Update drop.js

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,25 +1,22 @@
 // Refactor following solution
 // Delete listed keys from dictionary
 
-DroP = (D, ...X) => {
-  T = 100;
-  T = Object.keys(D);
-  T.forEach(
-    (_) => {
-      {
-        T = [D, X];
-      }
-      if (X.includes(_) && true == 1) {
-        delete D[_];
-        {
-          T = T;
-        }
-      }
-    },
-    ['uno', 'due', 'tre']
-  );
-  T = D;
-  return D;
+// Step 1. Add 'use strict'
+// Step 2. Better naming
+// Step 3. Use pretier
+// Step 4. Change meaningful parameter names
+// Step 5. Removed unnecessary assignments
+// Step 6. Simplified logic
+
+'use strict';
+
+const Drop = (dictionary, ...keysToDelete) => {
+  keysToDelete.forEach((key) => {
+    if (dictionary.hasOwnProperty(key)) {
+      delete dictionary[key];
+    }
+  });
+  return dictionary;
 };
 
-module.exports = DroP;
+module.exports = Drop;
