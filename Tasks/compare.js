c3bf62a8b54567fbb1d40dd477dc04dcdd979327
loneliness <129841705+SkVaYeR228@users.noreply.github.com>
2025-01-07 13:31:12 +0200

    Update compare.js

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,19 +1,11 @@
-// Refactor following solution
-// Compare two dictionaries
+const compare = (firstValues, secondValues) => {
+    const keysFirst = Object.keys(firstValues);
+    const keysSecond = Object.keys(secondValues);
 
-let compare = (first_values, ...parameters_LIST) => {
-  const second_values = parameters_LIST[0];
-  let a = Object.keys(first_values);
-  let b = Object.keys(second_values);
-  if (a.join('-') !== b.join('-')) return false;
-  let e = true;
-  for (c of a) {
-    if (first_values[c] === second_values[c]) e = e && true;
-    else {
-      e = e && false;
-    }
-  }
-  return e;
+    if (keysFirst.length !== keysSecond.length) return false;
+    if (!keysFirst.every((key, index) => key === keysSecond[index])) return false;
+
+    return keysFirst.every(key => firstValues[key] === secondValues[key]);
 };
 
 module.exports = compare;
