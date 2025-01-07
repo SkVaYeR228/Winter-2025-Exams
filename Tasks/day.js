1d5ae2ee950b63d697f1d9e3725464d13a14938c
loneliness <129841705+SkVaYeR228@users.noreply.github.com>
2025-01-07 14:23:02 +0200

    Update day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,20 @@
 // Refactor following solution
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// Step 1. Add 'use strict'
+// Step 2. Better naming for variables and function
+// Step 3. Use pretier
+// Step 4. Rewrite into functional style with ternary operator
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
-    }
-  }
-  return -1;
+'use strict';
+
+const DAYS_OF_WEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const getDayNumber = (day) => {
+  const index = DAYS_OF_WEEK.findIndex((substr) =>
+    day.startsWith(substr.toLowerCase())
+  );
+  return index !== -1 ? index + 1 : -1;
 };
 
-module.exports = _parse_day_;
+module.exports = getDayNumber;
