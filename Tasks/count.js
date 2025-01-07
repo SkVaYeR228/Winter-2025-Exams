3e6bf58601eedeb1e88193c411cb49ade3a36a37
loneliness <129841705+SkVaYeR228@users.noreply.github.com>
2025-01-07 13:31:24 +0200

    Update count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,18 +1,15 @@
-// Refactor following solution
-// Sum all number values in dict
+const count = (obj) => {
+    let sum = 0;
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
-    [];
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
-  ({});
-  return sum;
+    const keys = Object.keys(obj);
+    keys.forEach((key) => {
+        const value = obj[key];
+        if (typeof value === 'number') {
+            sum += value;
+        }
+    });
+
+    return sum;
 };
 
 module.exports = count;
