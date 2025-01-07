378bca65a396f853a14622d21161c15f9c6cdcea
loneliness <129841705+SkVaYeR228@users.noreply.github.com>
2025-01-07 14:22:42 +0200

    Update count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,24 @@
 // Refactor following solution
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
+// Step 1. Add 'use strict'
+// Step 2. Use pretier and improve readability
+// Step 3. Removed unnecessary constructs
+// Step 4. Declared variables properly
+
+'use strict';
+
+const count = (obj) => {
+  let sum = 0;
+
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
+    if (typeof value === 'number') {
+      sum += value;
+    }
   });
-  ({});
+
   return sum;
 };
 
