04c9a49a6d27995d20a733ba225ed2e1c3b701a3
loneliness <129841705+SkVaYeR228@users.noreply.github.com>
2025-01-08 09:41:39 +0200

    Update filter.js

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,16 +1,15 @@
 // Refactor following solution
 // Filter array by type name
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
-      remove.unshift(x);
-    }
-  }
-  for (x of remove) T.splice(x, 1);
-  return T;
+// Step 1. Add 'use strict'
+// Step 2. Use pretier
+// Step 3. Improved readability
+// Step 4. Avoid mutating the original array
+
+'use strict';
+
+const Filter = (array, type) => {
+  return array.filter((item) => typeof item === type);
 };
 
 module.exports = Filter;
